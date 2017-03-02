5d17c2afac184e4dd439d28117689de6530791c4
Zyphit <zyphit@users.noreply.github.com>
2017-03-02 12:57:16 -0800

    Add files via upload
    python script that reads the ISS current ground track position from a website and give it back in terms of a grid reference for an 8x16 LED matrix.  This will form the core of a function later in the project.

---
+++ isslatlong.py
@@ -0,0 +1,47 @@
+import requests
+import csv
+
+url = "http://astroviewer-sat2a.appspot.com/orbit"
+
+#reads url and stuffs contents into a list
+with requests.Session() as s:
+    download = s.get(url)
+    decoded_conten = download.content.decode('utf-8')
+    cr = csv.reader(decoded_conten.splitlines(), delimiter=',')
+    my_list = list(cr)
+
+#few steps to convert large list of strings into lat/long of ISS
+location = my_list[4]   #just the list index of interest
+location_str = ''.join(location)    #now it's a string.
+latlong = location_str.split(' ') # new list with space delimiters
+stn_lat = latlong[9]
+stn_long = latlong[7]
+print('The ISS is over ',stn_lat,' ',stn_long)
+
+
+# raw lat/longs seema little out from what the source website is reporting
+# my intended use for these numbers is okay with +/- 1 degree
+# because I'll be plotting the station position in boxes of 13x22 degrees
+# this script will eventually become a function that just returns the
+# current lat/long of the station.
+
+# now to convert lat/long to grid coordinates for 8x16 LED matrix
+import math
+#convert -55 to +55 --> 0 to +110
+grid_lat = float(stn_lat) + 55
+#divide by LED cell height, and round up for cell number.
+grid_y = math.ceil(grid_lat/13.75)
+
+#convert -180 to + 180 --> 0 to +360
+grid_long = float(stn_long) + 180
+#divide by LED cell weidth, and round up for cell number.
+grid_x = math.ceil(grid_long/22.5)
+
+print('Map grid ',grid_x,' ',grid_y)
+
+# grid_y and grid_x to be reported to the arduino which will light
+# up the corresponding LED.
+
+# See about adding a timeout condition so that if the script is unable
+# to access the url then another character is sent to the arduino
+# and a "los telemetry" light is activated.
