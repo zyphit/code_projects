a4486c6b17810c800a5d4bf8a4342eb1ad779bde
zyphit <bryn.laidlaw@gmail.com>
2017-03-03 06:10:08 +0000

    like the last one, a bit more functionality, just playing around

--- gpioblink.py
+++ gpioblink.py
@@ -22,27 +22,38 @@ GPIO.output(redLED, GPIO.LOW)
 GPIO.output(blueLED, GPIO.LOW)
 GPIO.output(yellowLED, GPIO.LOW)
 
+
+def blinky():
+	for i in range((count*4), (count*2), -1):
+		GPIO.output(redLED, GPIO.HIGH)
+		time.sleep(i*0.002)
+		GPIO.output(redLED, GPIO.LOW)
+		GPIO.output(yellowLED, GPIO.HIGH)
+		time.sleep(i*0.002)
+		GPIO.output(yellowLED, GPIO.LOW)
+		GPIO.output(blueLED,GPIO.HIGH)
+		time.sleep(i*0.002)
+	#	GPIO.output(redLED, GPIO.LOW)
+		GPIO.output(blueLED, GPIO.LOW)
+	#	GPIO.output(yellowLED, GPIO.LOW)
+
 # blink loop
 while count < 10:
 	button_state = GPIO.input(button)
 	if button_state == GPIO.LOW:
-		GPIO.output(redLED, GPIO.HIGH)
+		blinky()#GPIO.output(redLED, GPIO.HIGH)
 		count = count + 1
-		time.sleep(0.2)
-	else:
-		GPIO.output(redLED, GPIO.LOW)
 	GPIO.output(redLED, GPIO.LOW)
 
-for i in range(10, 0, -1):
+for i in range(5):
 	GPIO.output(redLED, GPIO.HIGH)
-	time.sleep(i*0.002)
+	time.sleep(1)
 	GPIO.output(redLED, GPIO.LOW)
-	GPIO.output(yellowLED, GPIO.HIGH)
-	time.sleep(i*0.002)
-	GPIO.output(yellowLED, GPIO.LOW)
-	GPIO.output(blueLED,GPIO.HIGH)
-	time.sleep(i*0.002)
-	GPIO.output(redLED, GPIO.LOW)
+	GPIO.output(blueLED, GPIO.HIGH)
+	time.sleep(1)
 	GPIO.output(blueLED, GPIO.LOW)
+	GPIO.output(yellowLED, GPIO.HIGH)
+	time.sleep(1)
 	GPIO.output(yellowLED, GPIO.LOW)
+	
 GPIO.cleanup()
