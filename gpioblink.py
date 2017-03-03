fd64d5c60b0f7cbb253452bda10d572b66d7ecaf
zyphit <bryn.laidlaw@gmail.com>
2017-03-03 05:55:40 +0000

    updated gpioblink.py to include a momentary button for interactive fun!

--- gpioblink.py
+++ gpioblink.py
@@ -7,14 +7,15 @@ redLED = 18
 blueLED = 23
 yellowLED = 24
 button = 26
+count = 0
 
 # pin setup
 GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(FALSE)
+GPIO.setwarnings(False)
 GPIO.setup(redLED, GPIO.OUT)
 GPIO.setup(blueLED, GPIO.OUT)
 GPIO.setup(yellowLED, GPIO.OUT)
-GPIO.setup(button, GPIO.IN, GPIO.PUT_UP)
+GPIO.setup(button, GPIO.IN, GPIO.PUD_UP)
 
 # initial state for LEDs:
 GPIO.output(redLED, GPIO.LOW)
@@ -22,14 +23,17 @@ GPIO.output(blueLED, GPIO.LOW)
 GPIO.output(yellowLED, GPIO.LOW)
 
 # blink loop
-while TRUE:
+while count < 10:
 	button_state = GPIO.input(button)
-	if button_state == GPIO.HIGH:
+	if button_state == GPIO.LOW:
 		GPIO.output(redLED, GPIO.HIGH)
+		count = count + 1
+		time.sleep(0.2)
 	else:
 		GPIO.output(redLED, GPIO.LOW)
-"""
-for i in range(150, 0, -1):
+	GPIO.output(redLED, GPIO.LOW)
+
+for i in range(10, 0, -1):
 	GPIO.output(redLED, GPIO.HIGH)
 	time.sleep(i*0.002)
 	GPIO.output(redLED, GPIO.LOW)
@@ -42,4 +46,3 @@ for i in range(150, 0, -1):
 	GPIO.output(blueLED, GPIO.LOW)
 	GPIO.output(yellowLED, GPIO.LOW)
 GPIO.cleanup()
-"""
