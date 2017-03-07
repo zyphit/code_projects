5f5a22867fff37d616dc8853a8a88c63d9dec914
Zyphit <zyphit@users.noreply.github.com>
2017-03-07 09:58:52 -0800

    Add files via upload
    fewer bugs!  Mostly works as intended!

--- ISS_map/ISS_map.ino
+++ ISS_map/ISS_map.ino
@@ -9,26 +9,32 @@
 */
 
 
-int longlist[9];
-int latlist[9];
+int longlist[] = {0,0,0,0,0,0,0,0,0,0};
+int latlist[] = {0,0,0,0,0,0,0,0,0,0};
 int newlong = 0;
 int newlat = 0;
 
 
 void setup() {
-  for (int i = 0; i < 10; i++) {
-    longlist[i] = 0;
-    latlist[i] = 0;
-  }
   Serial.begin(9600);
-  Serial.println("Arduino is read!");
+  
+  for (int i = 0; i < 10; i++) {
+    longlist[i] = i;
+    latlist[i] = i;
+    Serial.print(longlist[i]);
+    Serial.print(" ");
+    Serial.println(latlist[i]);
+  }
+ 
+  Serial.println("Arduino is ready!");
 
 }
 
 void loop() {
-  getcoords();
-  updatecoords();
-  displaymatrix();
+ getcoords();
+ updatecoords();
+ displaymatrix();
+ //Serial.println("can you hear me now?");
 
 }
 
@@ -37,8 +43,8 @@ void updatecoords() {
   //check if newest coordinate is different than most recent coordinate
   //then update lat/long list with new coordinates.
 
-  if (newlong == longlist[0] || newlat == latlist[0]) {
-    //not a new coordinate
+  if (newlong == longlist[0] && newlat == latlist[0]) {
+    Serial.println("No new coordinates");
   }
   else {
     //shifts matrix coordinates up one index, then assigns new current coordinate as index 0.
@@ -46,7 +52,7 @@ void updatecoords() {
       longlist[i] = longlist[i - 1];
     }
     longlist[0] = newlong;
-    for (int i = 0; i > 0; i--) {
+    for (int i = 9; i > 0; i--) {
       latlist[i] = latlist[i - 1];
     }
     latlist[0] = newlat;
@@ -58,13 +64,17 @@ void getcoords() {
   while (Serial.available() > 0) {
     //look for the next valid interger in the incoming serial stream:
     //Should be sent in the form "x,y"
+    Serial.println();
     newlong = Serial.parseInt();
+    Serial.println(newlong);
     newlat = Serial.parseInt();
+    Serial.println(newlat);
   }
 }
 
 void displaymatrix() {
   // prints the current values for longist[i] and latlist[i]
+  Serial.println();
   for (int i = 0; i < 10; i++) {
     Serial.print("index ");
     Serial.print(i);
@@ -74,20 +84,8 @@ void displaymatrix() {
     Serial.println(latlist[i]);
   }
   Serial.println();
+ 
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
